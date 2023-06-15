46cd3e816afdd0416b16cb8452e21717b2b6e81e
Richard <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2023-06-15 16:45:46 +0100

    fixed ubuntu script

--- scripts/send_ubuntu.sh
+++ scripts/send_ubuntu.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Please provide a string as an argument."
+  exit 1
+fi
+
 urlencode() {
   local string="$1"
   local length="${#string}"
@@ -16,17 +22,11 @@ urlencode() {
 
   echo "$encoded_string"
 }
-if [ -z "$1" ]; then
-  echo "Please provide a string as an argument."
-  exit 1
-fi
+
 string="$1"
+url_encoded_string=$(urlencode "$string")
 gateway=$(ip route | awk '/default/ {print $3}')
 interface=$(ip route get $gateway | awk '/dev/ {print $3}')
 mac_address=$(ip link show dev $interface | awk '/ether/ {gsub(/:/,""); print $2}')
-ipLine=$(ifconfig | grep 192)
-ipSuffix=${ipLine:16:2}
-echo "IP address of $interface: $ipSuffix"
-echo "MAC address of $interface: $mac_address"
-
-echo "192.168.1.17:8080/$ipSuffix/$mac_address/Task%20Completed/$string"
+ip_address=$(hostname -I | awk '{print $1}' | awk -F. '{print $NF}')
+curl 192.168.1.17:8080/http/$ip_address/$mac_address/Task%20Completed/$url_encoded_string
