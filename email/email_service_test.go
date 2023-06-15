c8e940e3a8bc17879a78a30fc31d700f19ce0375
Richard <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2023-06-15 14:30:51 +0100

    added email builder

--- email/email_service_test.go
+++ email/email_service_test.go
@@ -1,7 +1,10 @@
 package email
 
 import (
+	"database/sql"
+	events "go-sns/models"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -18,4 +21,22 @@ func TestNewEmailService(t *testing.T) {
 	assert.Equal(t, 587, emailService.dialer.Port)
 	assert.Equal(t, "username@domain.co.uk", emailService.dialer.Username)
 	assert.Equal(t, "password", emailService.dialer.Password)
+}
+
+func TestMessageBuilder(t *testing.T) {
+	event := &events.Event{
+		Id:      1,
+		Ip_addr: "192.168.1.1",
+		Inserted_at: sql.NullTime{
+			Time:  time.Date(2021, 01, 01, 00, 00, 00, 00, time.UTC),
+			Valid: true,
+		},
+		Mac_addr: "00:00:00:00:00:00",
+		Subject:  "Test Subject",
+		Message:  "Test Message",
+	}
+	emailService := &EmailService{}
+	message := emailService.MessageBuilder(*event)
+	assert.Equal(t, "<body>\n    <p><b>A new event has been logged</b></p>\n    <p>Time: 2021-01-01 00:00:00</p>\n    <p>From ip: 192.168.1.1</p>\n    <p>From mac: 00:00:00:00:00:00</p>\n    <p>Message: Test Message</p>\n</body>", message)
+
 }
